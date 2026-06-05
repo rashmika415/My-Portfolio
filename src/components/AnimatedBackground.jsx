5ee90a9618b3e1b95b40ca43417b29d4147eaed2
rashmika415 <rashmikaprabodana415@gmail.com>
2026-06-06 02:02:17 +0530

    add animations

--- src/components/AnimatedBackground.jsx
+++ src/components/AnimatedBackground.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const AnimatedBackground = () => {
   return (
     <div className="fixed inset-0 -z-10 overflow-hidden pointer-events-none" aria-hidden="true">
+      <div className="bg-conic" />
       <div className="bg-orb bg-orb-cyan" />
       <div className="bg-orb bg-orb-lime" />
       <div className="bg-orb bg-orb-purple" />
